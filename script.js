2dddbb2fbde622c2464dd3cb4f827b19eec9a6aa
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-02-22 15:30:08 +0600

    toggle tab added

--- script.js
+++ script.js
@@ -12,8 +12,15 @@ let rejectedCount = document.getElementById('rejected');
 const cards = document.getElementById('cards');
 const cardsClild = cards.children.length;
 
+// step: 5.1 main Container
+const mainContainer = document.querySelector('main');
 
-// Step: 3.1
+//Step: 4.1 get tabs btn
+const allTab = document.getElementById('all-tab')
+const interviewTab = document.getElementById('interview-tab')
+const rejectedTab = document.getElementById('rejected-tab')
+
+// Step: 3.1 calculate count function
 function calculateCount() {
     // Step: 2 here we need to update total in 2 places so i create a loop and update total value
     for (const element of total) {
@@ -23,3 +30,33 @@ function calculateCount() {
     rejectedCount.innerText = rejectedList.length
 }
 calculateCount()
+
+//Step: 4 toggle
+function toggle(id) {
+    // add all text color
+    allTab.classList.add('text-[#64748B]');
+    interviewTab.classList.add('text-[#64748B]');
+    rejectedTab.classList.add('text-[#64748B]');
+
+    // removing all btn bg color and text color
+    allTab.classList.remove('bg-[#3B82F6]', 'text-white');
+    interviewTab.classList.remove('bg-[#3B82F6]', 'text-white');
+    rejectedTab.classList.remove('bg-[#3B82F6]', 'text-white');
+
+    // get id from param
+    const selected = document.getElementById(id)
+
+    // adding & removing text color and add bg + text color for current button
+    selected.classList.remove('text-[#64748B]')
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
+
+    // console.log(allTab);
+    // console.log(interviewTab);
+    // console.log(rejectedTab);
+}
+
+// Step: 5 we add event leistener on main tag so we can do event delegation
+mainContainer.addEventListener('click', function(event) {
+    // console.log(event);
+    
+})
